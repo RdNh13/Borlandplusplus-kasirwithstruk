f18f8046be2f1dfe6bd9376503e03e5106a0a0fc
I T R A <71095453+RdNh13@users.noreply.github.com>
2022-03-18 06:08:18 +0700

    menambahkan soal no 4

---
+++ Project_Final/soal4.py
@@ -0,0 +1,22 @@
+#Apabila data di input dari user 
+
+data_sorting = [int(input()) for i in range (1,9)]
+
+print("Data yang di Inputkan : ", data_sorting)
+data_sorting.sort()
+print("Data yang di Inputkan sudah di sort : ", data_sorting)
+
+
+
+
+
+
+
+
+#Apabila data telah di sediakan
+data = [11,4,5,2,5,6,93,29]
+
+print("Data Sebelum Di Sorting : ", data)
+
+data.sort()
+print("Data setelah di Sorting : ", data)
